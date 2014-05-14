3ffa4100c813d024039d677bc9d0bb0fa1e1e0c1
Micha <michamimosa@gmail.com>
2014-05-14 21:31:27 +0200

    candycourier: open a window

--- CandyCourier/src/main.cpp
+++ CandyCourier/src/main.cpp
@@ -1,7 +1,18 @@
 #include <stdio.h>
+#include <oxygarum.h>
+
+using namespace oxygarum;
 
 int main(void) {
 	printf("Hello Candy-World!\n");
+
+	SDLWindow *window = new SDLWindow("CandyCourier", 800,600);
+
+	while(1) {
+		
+		window->update();
+	}
+
 	return 0;
 }
 
